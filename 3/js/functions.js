44373b81036c86cc23fb44f180730f4d8a692c44
Keks <keks@htmlacademy.ru>
2026-03-31 22:03:16 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,20 @@
+function checkLengthString(string = '', number = 1) {
+  return string.length <= number;
+}
+
+function isPalindrome(string = '') {
+  const normalizeString = string.replaceAll(' ', '').toLowerCase();
+  const reverseString = normalizeString.split('').reverse().join('');
+  return normalizeString === reverseString;
+}
+
+function getNumbers(string = '') {
+  const str = String(string);
+  let result = '';
+  for(const sign of str) {
+    if(sign >= '0' && sign <= '9') {
+      result += sign;
+    }
+  }
+  return parseInt(result, 10);
+}
