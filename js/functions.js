75b1a4bb5d6bacc1042cfbdc08e8ea66d51cd0ac
MIKA <imresquer@gmail.com>
2026-04-18 14:22:51 +0300

    Добавляет функцию определения превышения рабочего времени

--- js/functions.js
+++ js/functions.js
@@ -19,4 +19,21 @@ function getNumbers(string = '') {
   return parseInt(result, 10);
 }
 
-export {checkLengthString, isPalindrome, getNumbers};
+function isMeetOvertime(startWork, endWork, startMeet, durationMeet) {
+
+  let startWorkTime = parseInt(startWork.split(':')[0], 10) * 60 + parseInt(startWork.split(':')[1], 10);
+
+  let endWorkTime = parseInt(endWork.split(':')[0], 10) * 60 + parseInt(endWork.split(':')[1], 10);
+
+  let startMeetTime = parseInt(startMeet.split(':')[0], 10) * 60 + parseInt(startMeet.split(':')[1], 10);
+
+  let endMeetTime = startMeetTime + durationMeet;
+
+  return startMeetTime >= startWorkTime && endMeetTime <= endWorkTime;
+}
+
+console.log(isMeetOvertime('08:00', '17:30', '14:00', 90)); // true
+console.log(isMeetOvertime('8:0', '10:0', '8:0', 120));     // true
+console.log(isMeetOvertime('08:00', '14:30', '14:00', 90)); // false
+console.log(isMeetOvertime('14:00', '17:30', '08:0', 90));  // false
+console.log(isMeetOvertime('8:00', '17:30', '08:00', 900)); // false
