459c5aaee1e820b888e513cf8586ebb19e7450ae
Keks <keks@htmlacademy.ru>
2026-04-18 12:57:44 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/functions.js
@@ -0,0 +1,22 @@
+function checkLengthString(string = '', number = 1) {
+  return string.length <= number;
+}
+
+function isPalindrome(string = '') {
+  const normalizeString = string.replaceAll(' ', '').toLowerCase();
+  const reverseString = normalizeString.split('').reverse().join('');
+  return normalizeString === reverseString;
+}
+
+function getNumbers(string = '') {
+  const str = String(string);
+  let result = '';
+  for(const sign of str) {
+    if(!isNaN(parseInt(sign, 10))) {
+      result += sign;
+    }
+  }
+  return parseInt(result, 10);
+}
+
+export {checkLengthString, isPalindrome, getNumbers};
