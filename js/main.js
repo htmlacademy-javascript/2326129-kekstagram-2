76817ea99f436327d1f61d8c95bccf5d584a548e
MIKA <imresquer@gmail.com>
2026-04-18 11:46:13 +0300

    Добавляет новые файлы

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+import { getRandomElement, getRandomNumber } from './utils';
+import {constData} from './data';
+
+const {NAMES, MESSAGES} = constData;
+
+const PHOTO_AMOUNT = 25;
+
 const Likes = {
   MIN: 15,
   MAX: 200,
@@ -8,63 +15,9 @@ const Comments = {
   MAX: 30
 };
 
-const NAMES = [
-  'Барсик',
-  'Кекс',
-  'Мурзик',
-  'Борис',
-  'Муся',
-  'Глаша',
-  'Ириска',
-  'Мэл',
-  'Черныш',
-  'Маша',
-  'Рудольф'
-];
-
-const PHOTO_AMOUNT = 25;
-
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-function getRandomNumber(minValue, maxValue, isRepeat = false) {
-  const lower = Math.min(minValue, maxValue);
-  const upper = Math.max(minValue, maxValue);
-
-  if (isRepeat) {
-    return () => Math.floor(Math.random() * (upper - lower + 1) + lower);
-  }
-
-  const numbers = [];
-  for (let i = lower; i <= upper; i++) {
-    numbers.push(i);
-  }
-
-  for (let i = numbers.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-  }
-  let index = 0;
-
-  return () => {
-    if (index < numbers.length) {
-      return numbers[index++];
-    }
-  };
-}
-
-function getRandomElement(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+let commentId = 1;
 
 function getComment() {
-  let commentId = 1;
   return () => {
     const avatarId = getRandomNumber(1, 6);
     return {
@@ -77,18 +30,16 @@ function getComment() {
 }
 
 function getPhoto() {
-  const likesAmount = getRandomNumber(Likes.MIN, Likes.MAX, true);
-  const commentsAmount = getRandomNumber(Comments.MIN, Comments.MAX, true);
+  const commentsAmount = Math.floor(Math.random() * (Comments.MAX - Comments.MIN + 1) + Comments.MIN);
+  const likesAmount = Math.floor(Math.random() * (Likes.MAX - Likes.MIN + 1) + Likes.MIN);
   let photoId = 1;
-  return () => {
-    const photo = {};
-    photo.id = photoId++;
-    photo.url = `photos/${photoId - 1}.jpg`;
-    photo.description = `${photoId - 1} photo`;
-    photo.likes = likesAmount();
-    photo.comments = Array.from({length: commentsAmount()}, getComment());
-    return photo;
-  };
+  return () => ({
+    id: photoId++,
+    url: `photos/${photoId - 1}.jpg`,
+    description: `${photoId - 1} photo`,
+    likes: likesAmount,
+    comments: Array.from({length: commentsAmount}, getComment())
+  });
 }
 
 const photoArr = Array.from({length: PHOTO_AMOUNT}, getPhoto());
---
+++ js/utils.js
@@ -0,0 +1,20 @@
+function getRandomNumber(minValue, maxValue) {
+  const lower = Math.ceil(Math.min(minValue, maxValue));
+  const upper = Math.floor(Math.max(minValue, maxValue));
+
+  let previous = -1;
+  return () => {
+    const result = Math.floor(Math.random() * (upper - lower + 1) + lower);
+    if(previous !== result) {
+      previous = result;
+      return result;
+    }
+    return result === upper ? lower : result + 1;
+  };
+}
+
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+export {getRandomElement, getRandomNumber};
