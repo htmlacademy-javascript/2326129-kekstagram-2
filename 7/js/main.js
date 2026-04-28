85d4c697f800dbd4877005d6b90d7305ecbdc6d3
Keks <keks@htmlacademy.ru>
2026-04-28 21:37:21 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,5 +1,5 @@
 import { getPhotos } from './generateData';
-import { pictures } from './thumbnails';
+import { createThumbnails } from './thumbnails';
 
-console.log(getPhotos());
-console.log(pictures);
+createThumbnails(getPhotos());
+
